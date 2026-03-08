b1e6117cc4924f75e09db00319a04504cca55a28
sofiasaless <sofiasaleswk@gmail.com>
2026-03-08 00:36:42 -0300

    feat: paymentstatus on employee cards

--- src/hooks/usePayment.ts
+++ src/hooks/usePayment.ts
@@ -1,38 +1,49 @@
 import { FilterData, PaymentService } from "@/services/payment.service";
 import { PagamentoPostRequestBody } from "@/types/pagamento.type";
+import { getFirstDayAtMounth, getToday } from "@/utils/date";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-export function useListPayments(employeeId: string, filter: FilterData) {
+export function useListPayments(
+  employeeId: string,
+  filter: FilterData = {
+    data_fim: getToday().toISOString(),
+    data_inicio: getFirstDayAtMounth().toISOString(),
+  },
+) {
   return useQuery({
     queryKey: ["payments", employeeId],
     queryFn: async () => {
       const result = await PaymentService.list(employeeId, filter);
-      return result
+      return result;
     },
     refetchOnWindowFocus: false,
-    refetchOnMount: false
-  })
+    refetchOnMount: false,
+  });
 }
 
 export function usePayment() {
   const queryClient = useQueryClient();
 
   const pay = useMutation({
-    mutationFn: ({employeeId, body}: {employeeId: string, body: PagamentoPostRequestBody}) => PaymentService.pay(employeeId, body),
+    mutationFn: ({
+      employeeId,
+      body,
+    }: {
+      employeeId: string;
+      body: PagamentoPostRequestBody;
+    }) => PaymentService.pay(employeeId, body),
 
     onSuccess: () => {
-      console.info('success for payment'),
-      queryClient.invalidateQueries({ queryKey: ["employees"] }),
-      queryClient.invalidateQueries({ queryKey: ["payments"] })
+      queryClient.invalidateQueries({ queryKey: ["employees"] });
+      queryClient.invalidateQueries({ queryKey: ["payments"] });
     },
 
     onError: () => {
-      console.error('error for payment')
-    }
-  })
+      console.error("error for payment");
+    },
+  });
 
   return {
-    pay
-  }
-
+    pay,
+  };
 }
---
+++ src/utils/date.ts
@@ -0,0 +1,7 @@
+export function getFirstDayAtMounth() {
+  return new Date(new Date().setDate(1))
+}
+
+export function getToday() {
+  return new Date()
+}
